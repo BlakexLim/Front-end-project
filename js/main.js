77bffb874aee9e5be309e61b4dc5a827cd25a4e7
Blake Lim <blakelim007@gmail.com>
2024-03-07 23:22:23 +0000

    fetch api vehicle data in main.ts

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-async function apiData(): Promise<void> {
+'use strict';
+async function apiData() {
   const response = await fetch('https://www.swapi.tech/api/starships/2');
   const data = await response.json();
   console.log(data);
