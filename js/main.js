de5d911b8ee5682599cea4018e13ca810c8daeac
Blake Lim <blakelim007@gmail.com>
2024-03-08 19:11:27 +0000

    Created dummy list to syle in CSS

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 'use strict';
-async function apiData() {
-  const response = await fetch('https://www.swapi.tech/api/starships/2');
-  const data = await response.json();
-  console.log(data);
+let starShips = [];
+async function getStarShips() {
+  try {
+    const response = await fetch('https://www.swapi.tech/api/starships/');
+    const data = await response.json();
+    console.log(data);
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
-apiData();
+getStarShips();
--- ts/main.ts
+++ ts/main.ts
@@ -1,6 +1,23 @@
-async function apiData(): Promise<void> {
-  const response = await fetch('https://www.swapi.tech/api/starships/2');
-  const data = await response.json();
-  console.log(data);
+// interface Starships {
+//   model: string;
+//   manufacturer: string;
+//   class: string;
+//   maxAtmSpd: number;
+//   hyperDriveRating: number;
+//   cost: number;
+// }
+
+// let starShips = []
+async function getStarShips(): Promise<void> {
+  try {
+    const response = await fetch('https://www.swapi.tech/api/starships/');
+    const data = await response.json();
+    console.log(data);
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
-apiData();
+getStarShips();
