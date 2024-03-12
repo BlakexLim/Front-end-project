f6fb19c39f245042d54f5b29fa3102a587cb9f7f
Blake Lim <blakelim007@gmail.com>
2024-03-12 03:19:00 +0000

    issue with getting specific api properties to show on webpage #1

--- ts/main.ts
+++ ts/main.ts
@@ -16,97 +16,110 @@ const $ul = document.querySelector('ul');
 if (!$ul) throw new Error('$ul query failed');
 const $li = document.querySelector('li');
 if (!$li) throw new Error('$li query failed');
+const $renderedLi = document.querySelector('.ship-name');
+if (!$renderedLi) throw new Error('$renderedLi query failed');
 const $rundown = document.querySelector('.rundown');
 if (!$rundown) throw new Error('$rundown query failed');
-
-// $starShip.addEventListener('click', (event: Event) => {
-//   const $eventTarget = event.target as HTMLLIElement;
-//   if ($eventTarget.tagName === 'LI') {
-//    console.log($eventTarget.tagName);
-//   }
+const $model = document.getElementsByClassName('.model');
+if (!$model) throw new Error('$model query failed');
 
 const apiUrl = 'https://www.swapi.tech/api/starships/';
 
-// async function getStarShip() {
-//   try {
-//     const response = await fetch(apiUrl);
-//     console.log(response);
-//     if (!response.ok) {
-//       throw new Error(`${response.status} failed to fetch data`);
-//     }
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     console.error('Error fetching data');
-//     throw error
-//   }
-// }
-// getStarShip();
-
 // fetch api data for starship
-fetch(apiUrl)
-  .then((response) => {
+// retrieve data, store response in array
+// loop thru array to cross ref id and starship name
+// if id and name matches, get properties of that starship
+async function getStarShip(): Promise<void> {
+  try {
+    const response = await fetch(apiUrl);
     if (!response.ok) {
-      throw new Error(`${response.status}`);
+      throw new Error(`${response.status} failed to fetch data`);
     }
-    return response.json();
-  })
-  .then((data) => {
+    // let shipArr: Object[] = []
+    const data = await response.json();
     data.results.forEach((starship: StarShipName) => {
-      getShipName(starship);
+      $ul?.appendChild(getShipName(starship));
+      // shipArr.push(getShipName(starship));
     });
-    data.results.forEach((starshipData: StarShipData) => {
-      $rundown.append(getShipData(starshipData));
+    // console.log(shipArr)
+    // for (let i = 0; i < shipArr.length; i++) {
+    //   if (shipArr[i] === response) {
+
+    //   }
+    // }
+    data.results.forEach((starship: StarShipData) => {
+      console.log(starship);
+      $rundown?.append(getShipData(starship));
     });
-  })
-  .catch((error) => {
-    console.error('Error', error);
-  });
+  } catch (error) {
+    console.error('Error fetching data');
+    throw error;
+  }
+}
+getStarShip();
 
 // render starship name on webpage
-function getShipName(starship: StarShipName): void {
+function getShipName(starship: StarShipName): HTMLLIElement {
   const $shipName = document.createElement('li');
   $shipName.setAttribute('class', 'ship-name');
   $shipName.textContent = starship.name;
-  $ul?.appendChild($shipName);
+  return $shipName;
 }
 
 // render starship data on webpage
-function getShipData(starshipData: StarShipData): HTMLUListElement {
-  const $shipContainer = document.createElement('ul');
-
-  const $shipList = document.createElement('li');
-  $shipList.setAttribute('class', 'ship-data');
+function getShipData(starship: StarShipData): HTMLLIElement {
+  const $shipContainer = document.createElement('li');
+  $shipContainer.setAttribute('class', 'ship-data ship-name');
 
   const $shipModel = document.createElement('p');
-  $shipModel.textContent = starshipData.model;
+  $shipModel.textContent = starship.model;
 
   const $shipManufacturer = document.createElement('p');
-  $shipManufacturer.textContent = starshipData.manufacturer;
+  $shipManufacturer.textContent = starship.manufacturer;
 
   const $shipClass = document.createElement('p');
-  $shipClass.textContent = starshipData.class;
+  $shipClass.textContent = starship.class;
 
   const $shipMaxSpd = document.createElement('p');
-  $shipMaxSpd.textContent = starshipData.maxAtmSpd.toString();
+  $shipMaxSpd.textContent = starship.maxAtmSpd.toString();
 
   const $shipHypDrive = document.createElement('p');
-  $shipHypDrive.textContent = starshipData.hyperDriveRating.toString();
+  $shipHypDrive.textContent = starship.hyperDriveRating.toString();
 
   const $shipCost = document.createElement('p');
-  $shipCost.textContent = starshipData.cost.toString();
+  $shipCost.textContent = starship.cost.toString();
 
   const $fleetBtn = document.createElement('button');
   $fleetBtn.setAttribute('class', 'add-to-fleet');
   $fleetBtn.textContent = 'Add to fleet';
 
-  $shipContainer.appendChild($shipList);
-  $shipList.appendChild($shipModel);
-  $shipList.append($shipManufacturer);
-  $shipList.append($shipClass);
-  $shipList.append($shipMaxSpd);
-  $shipList.append($shipHypDrive);
-  $shipList.append($shipCost);
-  $shipList.append($fleetBtn);
+  $shipContainer.appendChild($shipModel);
+  $shipContainer.append($shipManufacturer);
+  $shipContainer.append($shipClass);
+  $shipContainer.append($shipMaxSpd);
+  $shipContainer.append($shipHypDrive);
+  $shipContainer.append($shipCost);
+  $shipContainer.append($fleetBtn);
   return $shipContainer;
 }
+
+async function selectShip(uid: number): Promise<void> {
+  const shipApi = `https://www.swapi.tech/api/starships/${uid}`;
+  console.log(shipApi);
+  try {
+    const response = await fetch(shipApi);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch property with ID ${uid}`);
+    }
+    const propertyData = await response.json();
+    return propertyData;
+  } catch (error) {
+    console.error('Error fetching property data');
+    throw error;
+  }
+}
+selectShip(2);
+
+$ul.addEventListener('click', (event: Event) => {
+  console.log(event.target);
+});
